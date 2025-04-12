8630fbf26764604873c2110cc46b8190b4597329
ictc user59 <ictcuser59@usmeducph.onmicrosoft.com>
2025-04-12 18:46:16 +0500

    002

--- reserba/urls.py
+++ reserba/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from accounts.views import UserViewSet, ShopViewSet, ServiceViewSet, BookingViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -12,4 +16,7 @@ router.register(r'bookings', BookingViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/', include('accounts.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
