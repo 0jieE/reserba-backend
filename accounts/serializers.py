b0cfa807ffc85a9e12496fa4b8747797d45ef757
ictc user59 <ictcuser59@usmeducph.onmicrosoft.com>
2025-04-12 20:20:30 +0500

    003

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import User, Shop, Service, Booking
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -21,3 +22,13 @@ class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = '__all__'
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'full_name', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.hashers import make_password
 from .models import User, Shop, Service, Booking
-from .serializers import UserSerializer, ShopSerializer, ServiceSerializer, BookingSerializer
+from .serializers import UserSerializer, ShopSerializer, ServiceSerializer, BookingSerializer, RegisterSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -17,3 +21,11 @@ class ServiceViewSet(viewsets.ModelViewSet):
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
